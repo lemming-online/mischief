3f088fd7df1e8ac8eec651c570fbf21bb5866732
Matthew Ess <mess@yelp.com>
2017-10-01 13:41:40 -0400

    All user methods except updating

--- mischief/resources.py
+++ mischief/resources.py
@@ -11,17 +11,24 @@ from mischief.schema import RegisteredUserSchema
 # noinspection PyMethodMayBeStatic
 @app.resource(endpoint='user_api', url='/users', pk='user_id')
 class UserResource(MethodView):
+    @app.use_schema(RegisteredUserSchema())
     def get(self, user_id):
-        pass
+        return mongo.db.users.find_one_or_404({'_id': user_id})
 
+    @app.use_schema(RegisteredUserSchema(), load=True)
     def put(self, user_id):
         pass
 
+    @app.use_schema(RegisteredUserSchema(), load=True)
     def patch(self, user_id):
         pass
 
     def delete(self, user_id):
-        pass
+        deleted = mongo.db.users.delete_one({'_id': user_id})
+        if deleted.acknowledged:
+            return {'success': True}
+        else:
+            return {'error': {'status_code': 500}}
 
 
 @app.resource(endpoint='users_api', url='/users')
