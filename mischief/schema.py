c7230fa3d9bc40e6ee8269618b3c0ea7eecd054b
Matthew Ess <mess@yelp.com>
2017-10-02 11:48:43 -0400

    Fix User document name

--- mischief/schema.py
+++ mischief/schema.py
@@ -13,39 +13,39 @@ from mischief.enum import RoleNames
 
 
 class MischiefSchema(Schema):
-    _mongo_field_name = NotImplementedError
+    _mongo_document_name = NotImplementedError
 
     _id = String(dump_only=True)
 
     @post_load
     def make_mongo(self, data):
         if data is None:
-            raise ValidationError('No user data provided')
+            raise ValidationError('No data provided')
         if data.get('_id'):
             data['_id'] = ObjectId(data['_id'])
         if request.method == 'POST':
-            return mongo.db[self._mongo_field_name].insert_one({**data})
+            return mongo.db[self._mongo_document_name].insert_one({**data})
         else:
-            return mongo.db[self._mongo_field_name].update_one({**data},
+            return mongo.db[self._mongo_document_name].update_one({**data},
                                                                {**data})
 
 
 class SectionSchema(MischiefSchema):
-    _mongo_field_name = 'sections'
+    _mongo_document_name = 'sections'
 
     name = String(required=True)
     location = String()
 
 
 class CourseSchema(MischiefSchema):
-    _mongo_field_name = 'courses'
+    _mongo_document_name = 'courses'
 
     name = String(required=True)
     sections = Nested(SectionSchema, many=True, default=[])
 
 
 class RoleSchema(MischiefSchema):
-    _mongo_field_name = 'roles'
+    _mongo_document_name = 'roles'
 
     title = String(
         default=RoleNames.mentee,
@@ -63,7 +63,7 @@ class UserSchema(MischiefSchema):
 
 
 class RegisteredUserSchema(UserSchema):
-    _mongo_field_name = 'sections'
+    _mongo_document_name = 'users'
 
     email = Email(unique=True, required=True)
     password = String(load_only=True, required=True)
